98ceb6294e419ac62c1661a97d57abebc9b28b12
dasteng <gdushtin@gmail.com>
2026-08-05 18:08:01 +0800

    adding some clickable image

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ window.addEventListener('blur', () => {
 window.addEventListener('focus', () => {
   document.title = "Dasteng";
 });
+
+const overlay = document.getElementById('overlay');
+const overlayImg = document.getElementById('overlayImg');
+
+document.querySelectorAll('.pictures img').forEach(img => {
+    img.addEventListener('click', () => {
+        overlayImg.src = img.src;
+        overlay.classList.add('active');
+    });
+});
+
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('active');
+});
